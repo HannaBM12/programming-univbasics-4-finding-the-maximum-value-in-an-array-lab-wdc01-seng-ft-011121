7c159c0c6fe2fdaaea197e3a33f3a75253e3ea3d
Hanna Mulugeta <hannabmulugeta@gmail.com>
2020-12-21 23:38:12 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,11 @@
 def find_max_value(array)
-  # Add your solution here
+  num = array[0]
+  counter = 0
+  while counter < array.length do
+    if array[counter] > num
+      num = array[counter]
+    end
+    counter +=1
+  end
+  num
 end
